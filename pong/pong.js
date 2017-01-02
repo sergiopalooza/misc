26c6300791d0f407472ac54fb773aad6c9138cfc
Sergio Flores <sergioflores836@gmail.com>
2017-01-01 21:46:39 -0600

    added controls to the paddle

--- pong/pong.js
+++ pong/pong.js
@@ -23,6 +23,7 @@ var step = function(){
 }
 
 var update = function(){
+	player.update();
 	ball.update(player.paddle, computer.paddle);
 };
 
@@ -48,6 +49,21 @@ Paddle.prototype.render = function(){
 	context.fillRect(this.x, this.y, this.width, this.height);
 }
 
+Paddle.prototype.move = function(x, y){
+	this.x += x;
+	this.y += y;
+	this.x_speed = x;
+	this.y_speed = y;
+
+	if(this.x < 0) {
+		this.x = 0;
+		this.x_speed = 0;
+	} else if (this.x + this.width > 400){
+		this.x = 0;
+		this.x_speed = 0;
+	}
+}
+
 function Player() {
 	this.paddle = new Paddle(175, 580, 50, 10);
 }
@@ -58,6 +74,19 @@ function Computer() {
 
 Player.prototype.render = function() {
 	this.paddle.render();
+}
+
+Player.prototype.update = function(){
+	for(var key in keysDown){
+		var value = Number(key);
+		if(value == 37){ //left arrow
+			this.paddle.move(-4, 0);
+		} else if (value == 39){
+			this.paddle.move(4, 0);
+		} else {
+			this.paddle.move(0,0);
+		}
+	}
 }
 
 Computer.prototype.render = function() {
@@ -94,10 +123,41 @@ Ball.prototype.update = function(paddle1, paddle2){
 		this.x = 395;
 		this.x_speed = -this.x_speed;
 	}
+
+	if(this.y < 0 || this.y > 600){ //a point was scored
+		this.x_speed = 0;
+		this.y_speed = 3;
+		this.x = 200;
+		this.y = 300;
+	}
+
+	if(top_y > 300) {
+	    if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+	      // hit the player's paddle
+	      this.y_speed = -3;
+	      this.x_speed += (paddle1.x_speed / 2);
+	      this.y += this.y_speed;
+	    }
+	  } else {
+	    if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+	      // hit the computer's paddle
+	      this.y_speed = 3;
+	      this.x_speed += (paddle2.x_speed / 2);
+	      this.y += this.y_speed;
+	    }
+	  }
 }
 
 var player = new Player();
 var computer = new Computer();
 var ball = new Ball(200, 300);
 
+var keysDown = {};
 
+window.addEventListener("keydown", function(event){
+	keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event){
+	delete keysDown[event.keyCode];
+})
